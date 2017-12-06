ce0f774fc633b478e3ef7a8162a3e781ecdefce6
Enner Pérez <ennerperez@gmail.com>
2017-12-06 13:10:10 -0400

    Initial develop commit

---
+++ MSBuild.Obfuscar/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("MSBuild.Obfuscar")]
+[assembly: AssemblyDescription("MSBuild targets for Obfuscar")]
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyCompany("LeXtudio")]
+[assembly: AssemblyProduct("MSBuild.Obfuscar")]
+[assembly: AssemblyCopyright("Copyright (C) LeXtudio 2015")]
+
+[assembly: AssemblyVersion("2.2.10.*")]
+[assembly: AssemblyFileVersion("2.2.10.0")]
+[assembly: NeutralResourcesLanguage("en")]
